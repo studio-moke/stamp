db1daeda1f9e6484d5e3eecd39a696bcb70daf26
studio-moke <yukipro1783@gmail.com>
2026-09-01 22:23:03 +0900

    Unify site URLs with trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stamp-moke.jp',
+  trailingSlash: 'always',
   experimental: {
     // Reuse previously rendered static pages when neither their data
     // nor their dependency graph has changed.
