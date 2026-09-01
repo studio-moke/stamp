68299a0d3ae2632bbe25b7c2bcef688ee4b43a88
studio-moke <yukipro1783@gmail.com>
2026-09-01 12:34:24 +0900

    Add image tools and localized palette pages to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -57,7 +57,8 @@ export function GET() {
   entries.push(...multilingualEntries("/tags/"));
   entries.push(...multilingualEntries("/qr-maker"));
   entries.push(...multilingualEntries("/chat-stamp-maker"));
-  entries.push(`  <url><loc>${escapeXml(`${SITE_URL}/color-palette/`)}</loc></url>`);
+  entries.push(...multilingualEntries("/color-palette/"));
+  entries.push(...multilingualEntries("/image-tools/"));
 
   for (const sticker of stickers) {
     entries.push(...multilingualEntries(`/stickers/${encodeURIComponent(sticker.id)}`));
