162c2730538ed72ae5fd7e80fa2d85e2161ff684
studio-moke <yukipro1783@gmail.com>
2026-09-04 18:52:43 +0900

    Stop auto-free publishing and add Pinterest background candidates
    * Disable automatic free-asset publication client
    
    * Hard-disable automatic free-asset publication API
    
    * Add Pinterest background candidate API
    
    * Route Pinterest candidate management API
    
    * Add Pinterest candidate review page

--- api/free.js
+++ api/free.js
@@ -2,10 +2,11 @@ import assets from "../server/api/free-assets.js";
 import list from "../server/api/free-list.js";
 import manga from "../server/api/free-manga-generated.js";
 import page from "../server/api/free-page-v2.js";
+import pinterest from "../server/api/pinterest-candidates.js";
 import preview from "../server/api/free-preview.js";
 import sitemap from "../server/api/free-sitemap.js";
 
-const routes = { assets, list, manga, page, preview, sitemap };
+const routes = { assets, list, manga, page, pinterest, preview, sitemap };
 
 export default async function handler(req, res) {
   const route = String(req.query?.route || "assets");
