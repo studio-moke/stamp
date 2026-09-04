b91ddc055ada896bd71a9762e505fdab4361e87e
studio-moke <yukipro1783@gmail.com>
2026-09-04 09:40:40 +0900

    Disable experimental incremental Astro builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://stamp-moke.jp',
   trailingSlash: 'always',
-  experimental: {
-    // Reuse previously rendered static pages when neither their data
-    // nor their dependency graph has changed.
-    incrementalBuild: true,
-  },
 });
