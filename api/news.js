6960920c5c15fdeb10ee4890318b1bebcf1d1114
studio-moke <yukipro1783@gmail.com>
2026-09-05 12:17:09 +0900

    Normalize internal page URLs in news API responses

--- api/news.js
+++ api/news.js
@@ -1,5 +1,7 @@
 import news from "../server/api/news.js";
+import { installInternalUrlNormalization } from "../server/api/_internal-url.js";
 
 export default async function handler(req, res) {
+  installInternalUrlNormalization(res);
   return news(req, res);
 }
