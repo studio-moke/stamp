7e8b19a04c44c79b35a6368fad6eca65b4e0a602
studio-moke <yukipro1783@gmail.com>
2026-09-05 12:17:19 +0900

    Normalize internal page URLs in tools APIs

--- api/tools.js
+++ api/tools.js
@@ -1,10 +1,12 @@
 import chatStamps from "../server/api/chat-stamps.js";
 import lineAuthor from "../server/api/line-author.js";
 import siteSearch from "../server/api/site-search.js";
+import { installInternalUrlNormalization } from "../server/api/_internal-url.js";
 
 const routes = { "chat-stamps": chatStamps, "line-author": lineAuthor, "site-search": siteSearch };
 
 export default async function handler(req, res) {
+  installInternalUrlNormalization(res);
   const route = String(req.query?.route || "");
   const target = routes[route];
   if (!target) return res.status(404).json({ error: "Unknown tools API route" });
