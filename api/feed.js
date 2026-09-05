91361efc2ced2575eb223c766d2fadc1f0caa3bd
studio-moke <yukipro1783@gmail.com>
2026-09-05 12:17:37 +0900

    Normalize internal page URLs in feed API

--- api/feed.js
+++ api/feed.js
@@ -1,5 +1,7 @@
 import feed from "../server/api/feed.js";
+import { installInternalUrlNormalization } from "../server/api/_internal-url.js";
 
 export default async function handler(req, res) {
+  installInternalUrlNormalization(res);
   return feed(req, res);
 }
