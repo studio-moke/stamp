00cc326547a8f00245cd82c88795fca5733891a4
studio-moke <yukipro1783@gmail.com>
2026-09-09 04:11:25 +0900

    Revert accidentally pushed digital material store changes

--- api/store-webhook.js
+++
@@ -1,9 +0,0 @@
-import storeWebhook from "../server/api/store-webhook-v2.js";
-
-export const config = {
-  api: { bodyParser: false }
-};
-
-export default async function handler(req, res) {
-  return storeWebhook(req, res);
-}
--- api/store.js
+++
@@ -1,7 +0,0 @@
-import store from "../server/api/store-v2.js";
-import { installInternalUrlNormalization } from "../server/api/_internal-url.js";
-
-export default async function handler(req, res) {
-  installInternalUrlNormalization(res);
-  return store(req, res);
-}
